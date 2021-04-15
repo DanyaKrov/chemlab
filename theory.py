7cae45f31a144e8daab8818a4ed99edd31f572b3
DanyaKrov <74681279+DanyaKrov@users.noreply.github.com>
2021-04-15 19:39:13 +0300

    Исравление
    Исправление поиска файла

--- theory.py
+++ theory.py
@@ -3,26 +3,22 @@ import os.path
 
 
 def collect(text):
-    el1, el2 = text
     #text = main_part(text) пока ещё не готово(
     con = sqlite3.connect(load_path('data.sqlite'))
     cur = con.cursor()
-    result = cur.execute(f"""SELECT descrp FROM Chains AS c
-    WHERE el1 LIKE '{el1}' AND  WHERE el2 LIKE '{el2}'""") #поиск по бд searching data base
+    result = cur.execute(f"""SELECT descrp FROM Theory AS t
+    WHERE title LIKE '{text}'""") #поиск по бд searching data base
     return result
     con.close()
 
 
 
-def main_part(s, s2): #кто-то может написать индексы не правильно, тогда программа все равно правильно сработает
-    s3 = s1 = ''
+def main_part(s): #кто-то может написать индексы не правильно, тогда программа все равно правильно сработает
+    s1 = ''
     for j in s:
         if j.isalpha() :
             s1 += j
-    for j in s2:
-        if j.isalpha() :
-            s3 += j
-    return (s1, s3)
+    return s1
 
 
 def load_path(name):
